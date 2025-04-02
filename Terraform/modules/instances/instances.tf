4ab4fc3ba71d61041066fb20344dde035e1cfaa4
PyeongBee <pyeongbee0806@gmail.com>
2025-04-02 21:52:47 +0900

    ebs 삭제 불가하게 수정

--- Terraform/modules/instances/instances.tf
+++ Terraform/modules/instances/instances.tf
@@ -26,7 +26,20 @@ resource "aws_ebs_volume" "gitlab_data" {
   size              = 20
   type              = "gp3"
 
+  lifecycle {
+    prevent_destroy = true
+  }
+
   tags = {
     Name = "${var.prefix}-gitlab-data-volume-${var.postfix}"
   }
+}
+
+# GitLab 인스턴스와 연결
+resource "aws_volume_attachment" "gitlab_data_attach" {
+  device_name  = "/dev/xvdf"
+  volume_id    = aws_ebs_volume.gitlab_data.id
+  instance_id  = aws_instance.gitlab.id
+  force_detach = true
+  skip_destroy = false
 }

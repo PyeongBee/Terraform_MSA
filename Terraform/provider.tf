02025515badb65939dcfdfd6c795d74c58884af0
PyeongBee <pyeongbee0806@gmail.com>
2025-04-08 15:19:04 +0900

    테라폼 백엔드 구성

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,4 +1,12 @@
-terraform {}
+terraform {
+  backend "s3" {
+    bucket         = "hans-s2s-s3-infra-tf-state"
+    key            = "dev/webapp/terraform.tfstate"
+    region         = "ap-northeast-2"
+    dynamodb_table = "hans-s2s-terraform-lock"
+    encrypt        = true
+  }
+}
 
 provider "aws" {
   region = var.region

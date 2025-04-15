0c0522604545a6fe2ababd22d6cb70cbf8a751ea
PyeongBee <pyeongbee0806@gmail.com>
2025-04-15 18:03:13 +0900

    provider k8s, helm 관련 정보 명시

--- Terraform/data.tf
+++ Terraform/data.tf
@@ -2,6 +2,14 @@ data "aws_secretsmanager_secret_version" "secrets" {
   secret_id = "hans_s2s_secrets"
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = "hans-s2s-eks-cluster-tf"
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = "hans-s2s-eks-cluster-tf"
+}
+
 locals {
   secret_data        = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)
   admin_access_cidrs = jsondecode(local.secret_data["admin_access_cidrs"])
--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -20,11 +20,15 @@ provider "aws" {
 }
 
 provider "kubernetes" {
-  config_path = "~/.kube/config"
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }

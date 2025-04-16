39a0d24519af079ca9e38f1298b7e298c595af3d
PyeongBee <pyeongbee0806@gmail.com>
2025-04-16 17:39:47 +0900

    헬름 차트(통합 인그레스, 앱1, 앱2)

--- Terraform/modules/kubernetes/kubernetes.tf
+++ Terraform/modules/kubernetes/kubernetes.tf
@@ -69,7 +69,7 @@ resource "helm_release" "argocd" {
   namespace  = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  version    = "7.7.11"
+  version    = "7.8.23"
 
   create_namespace = true
 

4bae11f4d9860a780c0b97fdf3f17441e8deccb9
PyeongBee <pyeongbee0806@gmail.com>
2025-04-16 15:34:51 +0900

    ns default -> argocd + 전용 인그레스 생성(헬름, yaml 기반)

--- Terraform/modules/kubernetes/kubernetes.tf
+++ Terraform/modules/kubernetes/kubernetes.tf
@@ -66,7 +66,7 @@ resource "helm_release" "external_dns" {
 
 resource "helm_release" "argocd" {
   name       = "${var.prefix}-argocd-${var.postfix}"
-  namespace  = "default"
+  namespace  = "argocd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
   version    = "7.7.11"

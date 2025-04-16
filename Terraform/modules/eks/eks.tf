917acd8cfede59726e7616b9923dadad4671223c
PyeongBee <pyeongbee0806@gmail.com>
2025-04-16 18:35:18 +0900

    프로메테우스, 그라파나 모니터링 추가 + 노드그룹 증설

--- Terraform/modules/eks/eks.tf
+++ Terraform/modules/eks/eks.tf
@@ -48,9 +48,9 @@ resource "aws_eks_node_group" "eks_node_group" {
   subnet_ids      = [var.private_subnet_ids[2], var.private_subnet_ids[3]]
 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
-    min_size     = 1
+    desired_size = 2
+    max_size     = 3
+    min_size     = 2
   }
 
   launch_template {
